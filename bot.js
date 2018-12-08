13f8669afafbf7aefced8f8935ee85025c537f30
Kjrrrr <45720859+Kjrrrr@users.noreply.github.com>
2018-12-08 23:57:15 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+
+    bot = new Discord.Client({sisableEveryone: true})
+var prefix = "-"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('By Wenteed')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
+  
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("ايدي");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`Welcome To **Fún** Server ...`), 3000)        
+}
+});
+
+
+
+});
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
