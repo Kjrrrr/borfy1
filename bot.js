7d515eebad407dd2e4b71ca414ec3c3764cb1ab6
Kjrrrr <45720859+Kjrrrr@users.noreply.github.com>
2018-12-12 16:44:54 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,35 +1,55 @@
 const Discord = require('discord.js');
-const Client = new Discord.Client();
-Client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('By Wenteed')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 
-Client.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("512569035347460106");
-if(!mohamed) return;
-if(mohamed) {
-setTimeout(() => mohamed.send(`Welcome To **Fún** Server ...`), 3000)        
-}
-});
-});
+const Util = require('discord.js');
+    client = new Discord.Client({sisableEveryone: true})
+    Client.on('ready', () => {
+        console.log(`Logged in as ${client.user.tag}!`);
+        console.log('')
+        console.log('')
+        console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+        console.log(`[Start] ${new Date()}`);
+        console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+        console.log('')
+        console.log('╔[════════════════════════════════════]╗');
+        console.log(`Logged in as * [ " ${client.user.username} " ]`);
+        console.log('')
+        console.log('Informations :')
+        console.log('')
+        console.log(`servers! [ " ${client.guilds.size} " ]`);
+        console.log(`Users! [ " ${client.users.size} " ]`);
+        console.log(`channels! [ " ${client.channels.size} " ]`);
+        console.log('╚[════════════════════════════════════]╝')
+        console.log('By Wenteed')
+        console.log('╔[════════════]╗')
+        console.log(' Bot Is Online')
+        console.log('╚[════════════]╝')
+        console.log('')
+        console.log('')
+      
+
+ const devs = ['392414705873977346'];
+const adminprefix = "K";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'playing')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'watching')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'listen')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'streaming')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Taaino");
+      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+  }
+  });
+
+
 Client.login(process.env.BOT_TOKEN);
