77e20e8d86b42b8c86d31570e002e35cd784abb2
Kjrrrr <45720859+Kjrrrr@users.noreply.github.com>
2018-12-21 10:11:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,34 +2,6 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
     client = new Discord.Client({sisableEveryone: true})
-    Client.on('ready', () => {
-        console.log(`Logged in as ${client.user.tag}!`);
-        console.log('')
-        console.log('')
-        console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-        console.log(`[Start] ${new Date()}`);
-        console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-        console.log('')
-        console.log('╔[════════════════════════════════════]╗');
-        console.log(`Logged in as * [ " ${client.user.username} " ]`);
-        console.log('')
-        console.log('Informations :')
-        console.log('')
-        console.log(`servers! [ " ${client.guilds.size} " ]`);
-        console.log(`Users! [ " ${client.users.size} " ]`);
-        console.log(`channels! [ " ${client.channels.size} " ]`);
-        console.log('╚[════════════════════════════════════]╝')
-        console.log('By Wenteed')
-        console.log('╔[════════════]╗')
-        console.log(' Bot Is Online')
-        console.log('╚[════════════]╝')
-        console.log('')
-        console.log('')
-      
-const Discord = require('discord.js');
-
-const Util = require('discord.js');
-    client = new Discord.Client({sisableEveryone: true})
  const devs = ['392414705873977346'];
 const adminprefix = "K";
 client.on('message', message => {
