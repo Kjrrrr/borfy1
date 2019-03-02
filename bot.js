c53cf426a480f94d5d5c628b5e61e00cc447bf74
Kjrrrr <45720859+Kjrrrr@users.noreply.github.com>
2019-03-02 22:52:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
     client = new Discord.Client({sisableEveryone: true})
- const devs = ['392414705873977346'];
+ const devs = ['208091530676666369'];
 const adminprefix = "K";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
@@ -10,21 +10,26 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'playing')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'watching')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'listen')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'streaming')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/Taaino");
-      message.channel.sendMessage(`**  ${argresult} البتاع اتغير الى :white_check_mark:**`)
+    client.user.setGame(argresult, "https://www.twitch.tv/raid__1");
+      message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   }
   });
 
+client.on('ready', () => {
 
-Client.login(process.env.BOT_TOKEN);
+console.log('Kjrr.');
+
+});
+
+client.login(BOT_TOKEN);
