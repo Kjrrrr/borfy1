88394ef0bb64e0600834c96c61c6c341e4c83e80
Kjrrrr <45720859+Kjrrrr@users.noreply.github.com>
2019-04-01 20:18:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,25 +2,25 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
     client = new Discord.Client({sisableEveryone: true})
- const devs = ['208091530676666369'];
-const adminprefix = "K";
+ const devs = ['374692744301379584'];
+const adminprefix = "R";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'playing')) {
+  if (message.content.startsWith(adminprefix + 'pl')) {
     client.user.setGame(argresult);
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'watching')) {
+  if (message.content.startsWith(adminprefix + 'wa')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'listen')) {
+  if (message.content.startsWith(adminprefix + 'll')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'streaming')) {
+  if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/raid__1");
       message.channel.sendMessage(`**  ${argresult} اتغيرت حالتك الى :white_check_mark:**`)
   }
